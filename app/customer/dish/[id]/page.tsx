23f1ef05d597d337ea9a3c716a0bb54ecc5d2ae1
nkumar <atxsolutions2026@gmail.com>
2026-01-11 22:17:56 -0600

    solve vercel build error

--- app/customer/dish/[id]/page.tsx
+++ app/customer/dish/[id]/page.tsx
@@ -10,7 +10,7 @@ export default async function DishDetailPage({
 
   return (
     <AppShell activeHref="/customer">
-      <CustomerDishDetailApi dishId={id} />
+      <CustomerDishDetailApi dishId={id} slug={""} />
     </AppShell>
   );
 }
